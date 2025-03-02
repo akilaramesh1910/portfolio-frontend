831d187a350b77a55b27fb75e866a60c593e3909
akila r <akila.r@instavans.com>
2025-03-02 18:18:41 +0530

    styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -34,6 +34,10 @@ class Header extends Component {
               </span>
               <span style={{ color: theme.text }}>/&gt;</span>
             </NavLink>
+            <input className="menu-btn" type="checkbox" id="menu-btn" />
+            <label className="menu-icon" htmlFor="menu-btn">
+              <span className="navicon"></span>
+            </label>
             <ul className="menu" style={{ backgroundColor: theme.body }}>
               <li>
                 <NavLink
